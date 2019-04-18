6f95adc21b793dee831cff84d12f9f556659c229
Andy Xu <andyxu12@gmail.com>
2019-04-18 14:57:20 -0400

    Added image prep

--- SWCExtractor.py
+++ SWCExtractor.py
@@ -10,14 +10,16 @@ class SWCNode():
         
 class SWCExtractor():
     def extract(self, size, filePath):
-        mat = np.zeros((size),dtype = np.uint8)
+        mat = np.zeros((size), dtype = np.uint8)
+        mat = np.expand_dims(mat, axis = 3)
 
-        parent_dict, node_dict = self.generateTree(size, filePath)
-        self.drawTree(parent_dict,node_dict, mat)
+        parent_dict, node_dict = self._generateTree(size, filePath)
+        self._drawTree(parent_dict, node_dict, mat)
         
         return mat
     
-    def generateTree(self, size, swcfile):
+
+    def _generateTree(self, size, swcfile):
         parent_dict = dict()
         node_dict = dict()
         
@@ -40,7 +42,8 @@ class SWCExtractor():
                     
         return parent_dict, node_dict
 
-    def drawTree(self, parent_dict, node_dict, mat):
+
+    def _drawTree(self, parent_dict, node_dict, mat):
         for key in parent_dict.keys():
             
             if (key == -1): continue
@@ -48,15 +51,16 @@ class SWCExtractor():
             parent = node_dict[key]
 
             for child in parent_dict[key]:
-                self.drawBranch([parent.x,parent.y,parent.z], [child.x,child.y,child.z], mat)
+                self._drawBranch([parent.x,parent.y,parent.z], [child.x,child.y,child.z], mat)
+             
                 
-    def drawBranch(self, p1, p2, mat):
+    def _drawBranch(self, p1, p2, mat):
 
         p1 = np.around(p1)
         p2 = np.around(p2)
         
         if all(p1 == p2):
-            mat[p1[0], p1[1], p1[2]] = 255
+            mat[p1[2], p1[0], p1[1], 0] = 1
             return mat
         
         unit_v = (p2-p1)/(np.linalg.norm(p2-p1))
@@ -66,32 +70,34 @@ class SWCExtractor():
                 diff = x - p1[0]
                 y = diff * unit_v[1] / unit_v[0] + p1[1]
                 z = diff * unit_v[2] / unit_v[0] + p1[2]
-                mat[int(round(x)), int(round(y)), int(round(z))] = 255
+                mat[int(round(z)), int(round(x)), int(round(y)), 0] = 1
             
             for y in np.linspace(p1[1],p2[1],np.abs(p2[1] - p1[1]) + 1):
                 if unit_v[1] == 0 : break
                 diff = y - p1[1]
                 x = diff * unit_v[0] / unit_v[1] + p1[0]
                 z = diff * unit_v[2] / unit_v[1] + p1[2]
-                mat[int(round(x)), int(round(y)), int(round(z))] = 255
+                mat[int(round(z)), int(round(x)), int(round(y)), 0] = 1
 
             for z in np.linspace(p1[2],p2[2],np.abs(p2[2] - p1[2]) + 1):
                 if unit_v[2] == 0 : break
                 diff = z - p1[2]
                 x = diff * unit_v[0] / unit_v[2] + p1[0]
                 y = diff * unit_v[1] / unit_v[2] + p1[1]
-                mat[int(round(x)), int(round(y)), int(round(z))] = 255
+                mat[int(round(z)), int(round(x)), int(round(y)), 0] = 1
 
         except Exception as e:
             print(str(e))
 
         return mat
 
+
 if __name__ == '__main__':
-    import imageio
-    mat = SWCExtractor().extract((511, 511, 401), "neuron_data/1xppk+Dcr_01-AlstR_TRiP27280_005_btmorphed.swc")
-    imageio.imwrite('test.png', np.max(mat.T, axis = 0))
-    print("SWC Image Drawn!")
+    mat = SWCExtractor().extract((401, 511, 511), "neuron_data/1xppk+Dcr_01-AlstR_TRiP27280_005_btmorphed.swc")
+    print(mat.shape)
+    
+    #imageio.imwrite('test.png', np.max(mat.T, axis = 2))
+    #print("SWC Image Drawn!")
     #for i in range(0, 401, 10):
     #    imageio.imwrite("test" + str(i) + ".png", mat.T[i, :, :])
 
--- TIFFExtractor.py
+++ TIFFExtractor.py
@@ -1,21 +1,62 @@
 from skimage import io
+import skimage.measure
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
 import png
 
 class TIFFExtractor():
     
     def extractMatrix(self, filePath):
         imageMat = io.imread(filePath)
+        imageMat = imageMat[:, 0:-2, 0:-2]
         imageMat = imageMat * 16
-        return imageMat;
+        
+        pooledMat = np.zeros(shape=(8, 511, 511), dtype = np.uint16)
+        
+        for i in range(imageMat.shape[0]):
+            pooledMat[i] = self._maxPool(imageMat[i], kernel_size=2, stride=2, padding=0)
+            
+        #adds extra channel to matrix for DataProvider    
+        pooledMat = np.expand_dims(pooledMat, axis = 3)
+        
+        return pooledMat;
+    
+    
+    def _maxPool(self, A, kernel_size, stride, padding):
+        '''
+        2D Pooling
+    
+        Parameters:
+            A: input 2D array
+            kernel_size: int, the size of the window
+            stride: int, the stride of the window
+            padding: int, implicit zero paddings on both sides of the input
+            pool_mode: string, 'max' or 'avg'
+        '''
+        # Padding
+        A = np.pad(A, padding, mode='constant')
+    
+        # Window view of A
+        output_shape = ((A.shape[0] - kernel_size)//stride + 1,
+                        (A.shape[1] - kernel_size)//stride + 1)
+        kernel_size = (kernel_size, kernel_size)
+        A_w = as_strided(A, shape = output_shape + kernel_size, 
+                            strides = (stride*A.strides[0],
+                                       stride*A.strides[1]) + A.strides)
+        A_w = A_w.reshape(-1, *kernel_size)
+    
+        return A_w.max(axis=(1,2)).reshape(output_shape)
+    
         
 def main():
     extract = TIFFExtractor()
     mat = extract.extractMatrix("neuron_data/1xppk+Dcr_01-AlstR_TRiP27280_007 ch 1.tif")
+    print(mat.shape)
     
-    for i in range(mat.shape[0]):
-        png.from_array(mat[i], 'L').save("TIFF" + str(i) + ".png")
-    
-    print("TIFF Image Drawn!")
+    #for i in range(mat.shape[0]):
+    #    png.from_array(mat[i], 'L').save("TIFF" + str(i) + ".png")
+     
+    #print("TIFF Image Drawn!")
     
 main()
 
